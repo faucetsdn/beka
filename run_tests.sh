fed4fdb1a21d3248b1abdf0c7aff8199e2fe7ff0
Brad Cowie <brad@wand.net.nz>
2021-07-07 14:03:46 +1200

    Run pytype on all python versions we test against.
    Also refactors code health checks out of run_tests.sh.

--- run_tests.sh
+++ run_tests.sh
@@ -6,14 +6,10 @@ BEKA_ROOT=$(dirname "$FILE_NAME")
 
 PIP_INSTALL=0
 UNIT_TEST=0
-CODE_CHECK=0
 
 # allow user to skip parts of test
 while getopts "nuz" o "${BEKA_TESTS}"; do
   case "${o}" in
-        n)
-            CODE_CHECK=1
-            ;;
         u)
             UNIT_TEST=1
             ;;
@@ -40,20 +36,4 @@ if [ "$UNIT_TEST" == 1 ] ; then
     echo "=============== Running Unit Tests ================="
     time env PYTHONPATH="${BEKA_ROOT}" pytest -v --cov=beka \
         "${BEKA_ROOT}"/test/unit/test_*.py
-fi
-
-# ============================= Code Checks =============================
-if [ "$CODE_CHECK" == 1 ] ; then
-    if [ "$PIP_INSTALL" == 1 ] ; then
-        echo "=============== Installing Pypi Dependencies ================="
-        pip3 install --upgrade --cache-dir=/var/tmp/pip-cache \
-            -r "${BEKA_ROOT}/codecheck-requirements.txt"
-    fi
-
-    echo "=============== Running PyType ===================="
-    time PYTHONPATH="${BEKA_ROOT}" pytype --config "${BEKA_ROOT}/setup.cfg" \
-        "${BEKA_ROOT}/beka/"
-
-    echo "=============== Running Pylint ===================="
-    time "${BEKA_ROOT}/test/codecheck/pylint.sh"
 fi
