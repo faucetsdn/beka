fd731794ec766b369fe493c7dea04ec1d3b453a8
Josh Bailey <anarkiwi@users.noreply.github.com>
2021-09-20 10:13:07 +1200

    Update min_pylint.sh

--- test/codecheck/min_pylint.sh
+++ test/codecheck/min_pylint.sh
@@ -8,6 +8,6 @@ for file in "$@" ; do
     echo ""
     echo "------------------------------------------------------------------"
     echo "pylint report for ${file}"
-    pylint --fail-under=${MINRATING} -d import-error ${file} || \
+    pylint --fail-under=${MINRATING} -d import-error,consider-using-f-string ${file} || \
         (echo "pylint rating for ${file} is below minimum of ${MINRATING}" && exit 1)
 done
